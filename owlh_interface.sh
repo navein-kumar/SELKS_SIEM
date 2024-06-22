6df51f8561ea01efdbc8a32162ab04ecc931a763
Naveen-Kumar <30574883+navein-kumar@users.noreply.github.com>
2024-06-22 07:27:01 +0530

    Update owlh_interface.sh

--- owlh_interface.sh
+++ owlh_interface.sh
@@ -3,11 +3,11 @@ cat > /etc/rc.local <<\EOF
 #!/bin/sh -e
 modprobe -v dummy numdummies=0
 ip link add owlh type dummy
-ip link set owlh mtu 65535
+ip link set owlh mtu 65536
 ip link set owlh up
 EOF
 chmod 755 /etc/rc.local 
 modprobe -v dummy numdummies=0
 ip link add owlh type dummy
-ip link set owlh mtu 65535
+ip link set owlh mtu 65536
 ip link set owlh up
